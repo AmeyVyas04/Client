6c3f95a6ee14c17ecac22be67a32dcd0769dd6a6
Amey Vyas <your-email@example.com>
2025-03-16 11:52:48 +0530

    latest

--- Frontend/src/components/Downloadapp.jsx
+++ Frontend/src/components/Downloadapp.jsx
@@ -5,10 +5,10 @@ function Downloadapp() {
     <>
     <div className='flex max-w-screen-2xl container mx-auto md:px-10 -mt-20  px-4 flex-col items-center md:flex-row justify-between gap-10'>
        
-        <div className="flex flex-col items-center mt-10 relative ml-20">
+        <div className="flex flex-col items-center mt-10 relative md:ml-20">
             <img loading="lazy" src="/Call for action.png" alt="Call for Action" className='min-w-screen  cursor-pointer ' />
             
-            <div className="absolute bottom-4 flex mr-24   md:mr-96">
+            <div className="absolute bottom-4 flex mr-20   md:mr-96">
               <a href="https://clplazarus.page.link/q3Lh" target="_blank" rel="noopener noreferrer">
                 <img loading="lazy" src="/Google Play.png" alt="Google Play" className='w-24 md:w-96 hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer md:mb-10 md:ml-11 ' />
               </a>
